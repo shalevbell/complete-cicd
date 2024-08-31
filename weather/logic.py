09ee150be2ec723837a239e9929e1f61d1ac8c18
shalev bell <shalevbell123@gmail.com>
2024-08-31 16:17:13 +0300

    optimizations to app logic and main

--- weather/logic.py
+++ weather/logic.py
@@ -1,48 +1,61 @@
 import requests  # used to request api from the web
 from datetime import datetime  # Used to check today's day
-
+import os
 
 def location_to_api(user_input):
     """
-    Return the url api with the location the user inputted
-    :param user_input: Is the location the user inputted.
-    :return: The api url.
+    Constructs the API URL for the given location.
+
+    :param user_input: Location provided by the user.
+    :return: The API URL as a string.
     """
-    api = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{user_input}?" \
-          f"unitGroup=metric&include=days%2Ccurrent%2Chours&key=[your_key_here]&contentType=json"
-    return api
+    api_key = os.getenv('API_KEY')
+    if not api_key:
+        raise EnvironmentError("API_KEY environment variable is not set")
+    
+    api_url = (
+        f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{user_input}?"
+        f"unitGroup=metric&include=days%2Ccurrent%2Chours&key={api_key}&contentType=json"
+    )
+    return api_url
 
 
-def api_to_dict(api_data):
+def api_to_dict(api_data: requests.Response):
     """
-    Return a dictionary with relevant info from api
-    :param api_data: Is the api url.
-    :return: A dictionary with all relevant data from the api.
+    Parses the API response and extracts relevant weather information.
+
+    :param api_response: The response object from the API request.
+    :return: A dictionary with relevant weather data.
+    :raises ValueError: If the API response is invalid.
     """
+    try:
+        data = api_data.json()
+    except ValueError:
+        raise ValueError("Invalid JSON response from API")
+    
     today = datetime.today().strftime('%A')  # today's date.
-    d1 = ["Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", ]
-    i = d1.index(today)  # the index of today's date.
-    days = d1[i:]  # select all days from before today.
-    days.extend((d1[:i]))  # append all the days to the end of the list.
+    days = ["Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", ]
+    i = days.index(today)
+    days = days[i:] + days[:i]
 
-    try:  # Tries to access the data as a json, if not a valid location raises an error.
-        data_dict = {
-            "c_location": api_data.json()["resolvedAddress"],
-            "c_temp": api_data.json()["currentConditions"]["temp"],
-            "c_humidity": api_data.json()["currentConditions"]["humidity"],
-            "c_uv": api_data.json()["currentConditions"]["uvindex"],
-            "c_sunrise": api_data.json()["currentConditions"]["sunrise"],
-            "c_sunset": api_data.json()["currentConditions"]["sunset"],
-            "c_description": api_data.json()["currentConditions"]["conditions"]
-        }
-    except:
-        raise TypeError
+    data_dict = {
+        "c_location": data["resolvedAddress"],
+        "c_temp": data["currentConditions"]["temp"],
+        "c_humidity": data["currentConditions"]["humidity"],
+        "c_uv": data["currentConditions"]["uvindex"],
+        "c_sunrise": data["currentConditions"]["sunrise"],
+        "c_sunset": data["currentConditions"]["sunset"],
+        "c_description": data["currentConditions"]["conditions"]
+    }
 
-    for x in range(7):  # Updates dict with temp in day and night for each day of the week.
-        data_dict.update({
-            days[x]: (api_data.json()["days"][x]["hours"][11]["temp"], api_data.json()["days"][x]["hours"][23]["temp"])
-        })
-    return data_dict
+    for x in range(7):
+        day_data = data["days"][x]
+        data_dict[days[x]] = (
+            day_data["hours"][11]["temp"], 
+            day_data["hours"][23]["temp"]
+        )
+
+    return day_data
 
 
 def location_to_dict(location):
@@ -52,5 +65,8 @@ def location_to_dict(location):
     :param location: The location the user inputted.
     :return: A dict with all relevant data from the api.
     """
-    api1 = location_to_api(location)
-    return api_to_dict(requests.get(api1))
+    api_url = location_to_api(location)
+    response = requests.get(api_url)
+    response.raise_for_status()
+
+    return api_to_dict(response)
--- weather/main.py
+++ weather/main.py
@@ -1,33 +1,43 @@
-from flask import Flask, render_template, request  # needed to use flask.
-from app import *  # imports all functions from app.py.
+from flask import Flask, render_template, request
 import json
 from datetime import date
-from os import listdir
+from os import listdir, environ
 from os.path import isfile, join
-import os
+from logic import *
 
 weather_app = Flask(__name__)  # create a new application instance called weather_app.
-bg = os.environ['BG_COLOR'] # Set bg as env variable BG_COLOR
+
+BG_COLOR = environ.get('BG_COLOR', '#e6dabc') # Set bg as env variable BG_COLOR
 
 
 @weather_app.route('/', methods=('GET', 'POST'))  # Sets an app rout and gives it GET & POST methods.
 def search():
-    if request.method == "POST":  # checks if the user is sending a POST request.
-        if request.form['submit_button'] == 'history':  # If user presses history button then execute
-            files = [f for f in listdir("static/history/") if isfile(join("static/history/", f))]
-            return render_template('weather_history.html', history=files, bg=bg)
-        elif request.form['submit_button'] == 'check':  # If user presses search button then execute
-            try:  # Checks if location_to_dict raised a typeError because the location was not valid.
-                location = request.form["input_location"]
+    if request.method == 'POST':
+        button = request.form.get('submit_button')
+        
+        if button == 'history':
+            # Retrieve list of history files
+            history_files = [
+                f for f in listdir('static/history/')
+                if isfile(join('static/history/', f))
+            ]
+            return render_template('weather_history.html', history=history_files, bg=BG_COLOR)
+        
+        elif button == 'check':
+            location = request.form.get('input_location')
+            try:
+                # Convert location to dictionary and save to file
                 location_dict = location_to_dict(location)
-                with open(f"static/history/{location}:{date.today()}.json", 'w') as outfile:
+                file_path = f'static/history/{location}:{date.today()}.json'
+                with open(file_path, 'w') as outfile:
                     json.dump(location_dict, outfile, indent=4)
-            except TypeError:  # send user to Error page.
-                return render_template('weather_error.html', bg=bg)
+                
+                return render_template('weather_main.html', location_dict=location_dict, bg=BG_COLOR)
+            except TypeError:
+                # Handle invalid location
+                return render_template('weather_error.html', bg=BG_COLOR)
 
-        return render_template('weather_main.html', location_dict=location_dict, bg=bg)
-
-    return render_template('weather_search.html', bg=bg)
+    return render_template('weather_search.html', bg=BG_COLOR)
 
 
 if __name__ == "__main__":
